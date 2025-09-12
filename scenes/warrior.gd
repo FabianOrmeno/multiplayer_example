611d3f04aa633c3047d0f0289fd29530721d8927
FabianOrmeno <fabian.ormeno@ug.uchile>
2025-09-12 13:30:07 -0300

    feat: se añadio el escudo para el guerrero

--- scenes/warrior.gd
+++ scenes/warrior.gd
@@ -1,9 +1,33 @@
 extends Player
 class_name  Warrior
+@onready var shield: Node2D = $shield
+@onready var cant_attack: bool = false
+
+func setup(player_data: Statics.PlayerData):
+	super.setup(player_data)
+	shield.setup(player_data)
+	shield.hide()
+	
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	if is_multiplayer_authority():
+		if Input.is_action_just_pressed("second action"):
+			weapon.hide()
+			shield.show()
+			cant_attack = true
+		if Input.is_action_just_released("second action"):
+			weapon.show()
+			shield.hide()
+			cant_attack = false
+			
+		shield.rotation = global_position.direction_to(get_global_mouse_position()).angle()
 
 func primary_action():
+	if cant_attack:
+		return
 	swing.rpc()
 	Debug.log("guerrero")
-	
+
 func secondary_action():
-	block.rpc()
+	shield.rotation = global_position.direction_to(get_global_mouse_position()).angle()
+	shield.block()
