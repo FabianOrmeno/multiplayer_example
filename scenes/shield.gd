32f11fa31fb97fe4c8f2e116f6aab43797fb9bd6
Nicohylian <nicolasferoe@gmail.com>
2025-10-10 13:21:13 -0300

    feat: el escudo puede reflejar bolas de fuego y de hielo

--- scenes/shield.gd
+++ scenes/shield.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
 @onready var colition_shield: CollisionShape2D = $Hitbox/CollisionShape2D
+@onready var hitbox: Hitbox = $Hitbox
 
 var data: Statics.PlayerData = null
 
@@ -9,6 +10,8 @@ var data: Statics.PlayerData = null
 func _ready() -> void:
 	if data:
 		setup(data)
+	if is_multiplayer_authority():
+		hitbox.area_entered.connect(reflect)
 
 
 func setup(player_data: Statics.PlayerData):
@@ -20,6 +23,7 @@ func setup(player_data: Statics.PlayerData):
 		data = player_data
 
 
+
 func block():
 	#Activar collision de espada
 	colition_shield.disabled = false
@@ -28,4 +32,14 @@ func block():
 
 func _disable_sword_collision():
 	colition_shield.disabled = true
-	
+
+func reflect(area: Area2D) -> void:
+	Debug.log("area entered")
+	var fireball = area as FireBall
+	var iceball = area as Iceball
+	if fireball:
+		var angle = get_angle_to(fireball.global_position)
+		fireball.rotate(PI-angle*2)
+	elif iceball:
+		var angle = get_angle_to(iceball.global_position)
+		iceball.rotate(PI - angle*2)	
